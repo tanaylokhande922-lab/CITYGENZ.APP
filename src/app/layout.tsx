126a53af7099d1038590fee8e0646d5940fd9936
TANAY LOKHANDE <tanaylokhande922@gmail.com>
2025-11-02 08:07:03 +0000

    Error: useFirebase must be used within a FirebaseProvider.     at useFir

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,8 +23,8 @@ export default function RootLayout({
       <body className="font-body antialiased">
         <FirebaseClientProvider>
           {children}
+          <Toaster />
         </FirebaseClientProvider>
-        <Toaster />
       </body>
     </html>
   );
